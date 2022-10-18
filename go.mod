fa70d95a937a7869415c9e5e781d2c29b526c99c
andrey <andrey.aksenov2001@gmail.com>
2022-10-19 02:03:40 +0300

    sending with delay in progress

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/gocelery/gocelery v0.0.0-20201111034804-825d89059344 // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -25,6 +27,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b // indirect
+	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -2,38 +2,69 @@ package sender
 
 import (
 	"bytes"
+	"github.com/gocelery/gocelery"
+	"github.com/gomodule/redigo/redis"
 	"html/template"
 	"log"
 	"mailing/internal/dbsubs"
 	"net/smtp"
+	"os"
 	"strings"
+	"time"
 )
 
 var auth = smtp.PlainAuth("", "andrey.aksenov2001@gmail.com", "password", "smtp.gmail.com")
 var headers = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
 
-func SendEmail(sendtime string, tmpl string, subs []dbsubs.Subscriber) {
-	if sendtime == "now" {
-		log.Println("Sending...")
-		subject := strings.ToUpper(tmpl)
-		tmplPath := "templates/msg/" + tmpl + ".html"
-		t, err := template.ParseFiles(tmplPath)
+// create redis connection pool
+var redisPool = &redis.Pool{
+	Dial: func() (redis.Conn, error) {
+		c, err := redis.DialURL(os.Getenv("REDIS_URL"))
 		if err != nil {
-			log.Println(err)
+			return nil, err
 		}
+		return c, err
+	},
+}
 
-		msg := "Subject: " + subject + "\n" + headers + "\n\n"
+// initialize celery client
+var cli, _ = gocelery.NewCeleryClient(
+	gocelery.NewRedisBroker(redisPool),
+	&gocelery.RedisCeleryBackend{Pool: redisPool},
+	5, // number of workers
+)
 
+var taskName = "worker.send"
+
+func SendEmail(sendtime string, tmpl string, subs []dbsubs.Subscriber) {
+
+	subject := strings.ToUpper(tmpl)
+	tmplPath := "templates/msg/" + tmpl + ".html"
+	t, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	msg := "Subject: " + subject + "\n" + headers + "\n\n"
+	if sendtime == "now" {
 		for _, sub := range subs {
 			go sender(t, msg, sub)
 		}
 
 	} else {
 		log.Println("Later")
+		d, _ := time.Parse("2006-01-02 15:04", sendtime)
+		//d := t.Unix() - time.Now().Unix()
+		cli.Register("worker.send", sendWithDelay)
+		_, err = cli.Delay(taskName, d, t, msg, subs)
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
 
 func sender(t *template.Template, msg string, sub dbsubs.Subscriber) {
+	log.Println("Sending...")
 	var body bytes.Buffer
 
 	err := t.Execute(&body, sub)
@@ -51,5 +82,13 @@ func sender(t *template.Template, msg string, sub dbsubs.Subscriber) {
 	)
 	if err != nil {
 		log.Println(err)
+	}
+}
+
+func sendWithDelay(d time.Time, t *template.Template, msg string, subs []dbsubs.Subscriber) {
+	log.Println(time.Until(d))
+	time.Sleep(time.Until(d))
+	for _, sub := range subs {
+		sender(t, msg, sub)
 	}
 }
